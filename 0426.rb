1e81ba168d6cf8face4f0c19e6a8661166bb70b0
Byeongcheol Yoo <regalfaith@gmail.com>
2018-04-29 21:13:56 +0900

    edited from taeyean source

---
+++ 0426.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require './block'
+
+b = Blockchain.new
+
+get '/' do
+  message = ""
+  b.current_chain.each do |c|
+  message << "the index value is " + c['index'].to_s + "<br>"
+	message << "the nonce value is " + c['nonce'].to_s + "<br>"
+	message << "the time is " + c['time'].to_s + "<br>"
+	message << "last address is " + c['previous_block'].to_s + "<br>"
+	message << "my address is " + Digest::SHA256.hexdigest(c.to_s) + "<br>"
+	message << "<hr>"
+  end
+  
+  message
+end
+
+get '/mine' do
+  b.mining
+  "I found blocks"
+end
--- block.rb
+++ block.rb
@@ -1,5 +1,36 @@
-class BlockChain
+require 'digest'
 
-	attr_reader :chain, :transactions
+class Blockchain
 
+  def initialize
+  	block = {
+	  'index' => 1,
+	  'time' => Time.now,
+	  'nonce' => '유병철 짱',
+	  'previous_block' => 0
+	}
+    @chain = [block]
+  end
+
+  def mining
+	begin
+	  nonce = rand(100)
+	end while (nonce %2 != 0) || (nonce %5 != 0)
+	
+	block = {
+	  'index' => @chain.length + 1,
+	  'time' => Time.now,
+	  'nonce' => nonce,
+	  'previous_block' => Digest::SHA256.hexdigest(last_block.to_s)
+	}
+	@chain << block
+  end
+  
+  def current_chain
+    @chain
+  end
+  
+  def last_block
+    @chain[-1]
+  end
 end
